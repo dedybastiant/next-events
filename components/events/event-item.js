5881fb8f19a1d331e1ccfd46e0a2855d405be114
dedybastiant <dedybastian.t@gmail.com>
2023-07-14 06:13:41 +0700

    Create button for link, add icon and change some styling

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,9 @@
 import Link from "next/link";
 import classes from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import Button from "../ui/Button";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 function EventItem(props) {
   const { title, location, image, date, id } = props;
@@ -19,14 +23,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humandReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <spam>Explore Event</spam>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
