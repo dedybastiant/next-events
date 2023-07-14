60f786443077f4adc8b559a0751a012740f2a042
dedybastiant <dedybastian.t@gmail.com>
2023-07-14 07:34:38 +0700

    Add navigation for filtered event programmatically

--- components/event-search/event-search.js
+++ components/event-search/event-search.js
@@ -1,20 +1,33 @@
+import { useRef } from "react";
 import Button from "../ui/Button";
 import classes from "./event-search.module.css";
 
-function EventSearch() {
+function EventSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function sumitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={sumitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year"></label>
-          <select if="year">
+          <select if="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month"></label>
-          <select id="month">
+          <select id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
+import { useRouter } from "next/router";
 import EventSearch from "../components/event-search/event-search";
 import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../dummy-data";
 
 function HomePage() {
+  const router = useRouter();
   const featuredEvents = getFeaturedEvents();
+
+  function finEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath)
+  }
 
   return (
     <div>
-      <EventSearch />
+      <EventSearch onSearch={finEventsHandler}/>
       <EventList items={featuredEvents} />
     </div>
   );
